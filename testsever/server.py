cebe8bc429d72b478cb50308092318b61f8f3d2b
prajwaltr93 <prajwaltr93@gmail.com>
2018-11-11 10:58:01 +0530

    this is commit includes directkey.py addition to master

--- testsever/server.py
+++ testsever/server.py
@@ -1,8 +1,10 @@
 #!python
 
+import pickle
 import socket
 # to test latency
-#import datetime
+from directkeys import PressKey, W, A, S, D,ReleaseKey
+
 #fh = open(r'm:/serverlog.txt','a')
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 byname = socket.gethostname()
@@ -26,19 +28,75 @@ while True:
     #CSocket.send(str('dir').encode())
     while True:
         #dt = datetime.datetime.now()
-        data = CSocket.recv(100).decode()
-        if not data:
+        temp = CSocket.recv(100)
+        if not temp:
             break
         else:
             #fh.write(data+'-'+str(dt.minute)+':'+str(dt.second)+'\n')
-            print(data)
+            data = pickle.loads(temp)
+            r_v = int(data['Y'])
+            #find alternative to press keys
 
-    '''
-    while True:
-        if not (CSocket.recv(1000).decode()):
-            time.sleep(3)
-    #CSocket.send(str(input()).encode('utf-8'))
-    '''
+            if r_v < 0:
+                r_v = (-1*r_v)
+                if data['T'] and data['B']:
+                    PressKey(W)
+                    PressKey(S)
+                    PressKey(A)
+                    ReleaseKey(W)
+                    ReleaseKey(S)
+
+                elif data['T']:
+                    PressKey(W)
+                    PressKey(A)
+                    ReleaseKey(W)
+                    ReleaseKey(A)
+
+                elif data['B']:
+                    PressKey(S)
+                    PressKey(A)
+                    ReleaseKey(S)
+                    ReleaseKey(A)
+                else:
+                    PressKey(A)
+                    ReleaseKey(A)
+            elif r_v > 0:
+                if data['T'] and data['B']:
+                    PressKey(W)
+                    PressKey(S)
+                    PressKey(D)
+                    ReleaseKey(W)
+                    ReleaseKey(S)
+                    ReleaseKey(D)
+
+                elif data['T']:
+                    PressKey(W)
+                    PressKey(D)
+                    ReleaseKey(W)
+                    ReleaseKey(D)
+                elif data['B']:
+                    PressKey(S)
+                    PressKey(D)
+                    ReleaseKey(S)
+                    ReleaseKey(D)
+                else:
+                    PressKey(D)
+                    ReleaseKey(D)
+            else:
+                if data['T'] and data['B']:
+                    PressKey(W)
+                    PressKey(S)
+                    ReleaseKey(W)
+                    ReleaseKey(S)
+                elif data['T']:
+                    PressKey(W)
+                    ReleaseKey(W)
+                elif data['B']:
+                    PressKey(S)
+                    ReleaseKey(S)
+
+
+
     choice = input('Do you Want shut Down Server . (y/n):')
     if choice == 'y':
         break
